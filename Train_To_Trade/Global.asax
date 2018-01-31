5bbfd62a96153206fdb4d55711485433d9d5e29b
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-01-31 11:27:01 +0200

    New repo for train to trade

---
+++ Train_To_Trade/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Train_To_Trade.MvcApplication" Language="C#" %>
---
+++ Train_To_Trade/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Train_To_Trade.Startup))]
+namespace Train_To_Trade
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
