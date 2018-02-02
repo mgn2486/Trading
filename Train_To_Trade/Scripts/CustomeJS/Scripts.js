3cca1aeacde921599813326380c7cbaa9df6bf94
Masimba Gomba Ndadzibaya <masimban@sahomeloans.com>
2018-02-02 15:17:35 +0200

    Updated the banner

---
+++ Train_To_Trade/Scripts/CustomeJS/Scripts.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('img').hide();
+
+    function anim() {
+        $("#wrap img").first().appendTo('#wrap').fadeOut(3500).addClass('transition').addClass('scaleme');
+        $("#wrap img").first().fadeIn(3500).removeClass('scaleme');
+        setTimeout(anim, 3700);
+    }
+    anim();
+});
