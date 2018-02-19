f54ba55783ffc3fddfbb180d0445653dcd512a55
massy <massy19@gmail.com>
2018-02-19 05:30:39 +0200

    upfated the mail model

---
+++ Train_To_Trade/Models/WhatsAppModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Train_To_Trade.Models
+{
+    public class WhatsAppModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        public string emailaddress { get; set; }
+
+        [Required(ErrorMessage = "Whats app cell number is required")]
+        public decimal whatsAppCell { get; set; }
+        
+    }
+}
